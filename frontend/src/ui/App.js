cca3e4a941c5fa4a49501d5cfae75c2f7b35127e
Brett Gobel <brettg841@gmail.com>
2020-11-09 20:25:52 -0700

    added react router

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,3 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
+import { Home } from './Home'
+import { FourOhFour } from './FourOhFour'
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-export const App = () => ( <h1 className="text-info">Is this thing on?</h1> );
+
+export const App = () => (
+    <>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route component={FourOhFour} />
+            </Switch>
+        </BrowserRouter>
+
+    </>
+)
