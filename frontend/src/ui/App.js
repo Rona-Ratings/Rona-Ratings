b66ced072a7e4b4d5446022379d376f6eef19f1f
Josh Little <jlittle10@cnm.edu>
2020-11-11 16:57:27 -0700

    added react to front page

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -4,13 +4,13 @@ import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
 import {DetailRestaurant} from "./DetailRestaurant/DetailRestaurant";
-
+import {FrontPage} from "./FrontPage/FrontPage";
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path="/detail-restaurant" component={DetailRestaurant}/>
-                <Route exact path='/' component={Home} />
+                <Route exact path='/' component={FrontPage} />
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>
