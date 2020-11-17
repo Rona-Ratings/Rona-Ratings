7bf2a2594347bee2fa9ac926384805bb3a3c4dba
Brett Gobel <brettg841@gmail.com>
2020-11-16 17:06:54 -0700

    Tryna merge um all

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -5,12 +5,15 @@ import { FourOhFour } from './FourOhFour'
 import React from 'react'
 import {DetailRestaurant} from "./DetailRestaurant/DetailRestaurant";
 import { ListingPage } from "./ListingsPage/ListingPage";
+import { FrontPage} from "./FrontPage/FrontPage";
+
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path="/detail-restaurant" component={DetailRestaurant}/>
                 <Route exact path="/listing-Page" component={ListingPage}/>
+                <Route exact path="/FrontPage" component={FrontPage}/>
                 <Route exact path='/' component={Home} />
                 <Route component={FourOhFour} />
             </Switch>
