b21efde649237aebbfb00bfa5af216c03eab30fe
Brett Gobel <brettg841@gmail.com>
2020-11-20 12:38:30 -0700

    cards now even?

--- frontend/src/ui/restaurant-listings/restaurant-listings.js
+++ frontend/src/ui/restaurant-listings/restaurant-listings.js
@@ -32,43 +32,42 @@ export const RestaurantListings = () => {
                         </ul>
                     </nav>
                 </div>
-        <div>
+
             <div className="container">
-
-
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-lg-12 px-0">
-                            <div className="card">
-                                <div className={styles.cardHeader}>
-                                    <div className="card-header">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-lg-12 px-0">
+                        <div className="card">
+                            <div className={styles.cardHeader}>
+                                <div className="card-header">
                                     Taco Juan's Taco Shop
-                                    </div>
                                 </div>
+                            </div>
                                 <div className="col-lg-12">
                                     <div className="row">
-                                    <div className="col-lg-3 d-flex justify-content-center">
-                                    <img src={TacoPlaceHolder} className="card-img" alt="placeholder" />
-                                    </div>
-                                    <div className="col-lg-9 d-flex align-items-center">
-                                        <div className="card-body">
-                                            <h3 className="card-title">Restaurant Information:</h3>
-                                            <p className="card-text">
+                                        <div className="col-lg-3 d-flex justify-content-center">
+                                            <img src={TacoPlaceHolder} className="card-img" alt="placeholder" />
+                                        </div>
+                                        <div className="col-lg-9 d-flex align-items-center">
+                                            <div className="card-body">
+                                                <h3 className="card-title">Restaurant Information:</h3>
+                                                <p className="card-text">
                                                 <h4>Address and Hours:UNM Carri Tingley Hospital -</h4>
                                                 <p>1127 University Blvd, NE Albuquerque, NM 87102 Friday 8:00am - 2:00pm</p>
                                                 <h4>contact Information:</h4>
+                                                    <ul>
                                                 <li>Phone Number:505-319-5619</li>
                                                 <li>Delivery Option: Yes with a perches of $20.00 or more.</li>
                                                 <li>Patio Option: Yes</li>
-                                            </p>
+                                                    </ul>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                    </div>
                                 </div>
-
-                            </div>
                         </div>
-
-
+                    </div>
+                </div>
+            </div>
 
                                 <div className="footer">
                                     <div className="footer-content">
@@ -154,10 +153,7 @@ export const RestaurantListings = () => {
 
 
 
-                        </div>
 
-                </div>
-            </div>
 
 
             </>
