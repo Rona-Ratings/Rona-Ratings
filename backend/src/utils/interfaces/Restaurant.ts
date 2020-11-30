a56a95c3e997326443d245996b378ef2cbfc2dd3
Brett Gobel <brettg841@gmail.com>
2020-11-29 19:13:26 -0700

    adding datadownloader material

---
+++ backend/src/utils/interfaces/Restaurant.ts
@@ -0,0 +1,11 @@
+export interface Restaurant {
+    restaurantId: string |  null;
+    restaurantAddress: string;
+    restaurantImage: string;
+    restaurantName: string;
+    restaurantPhone: string;
+    restaurantServices: string;
+    restaurantYelpRating: number;
+    restaurantZip: string;
+
+}
